f6a592250a53a4e77e041976851e1200b0c0be6f
brucetech627 <66194009+brucetech627@users.noreply.github.com>
2021-04-22 13:43:55 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Just a basic script U can improvise lateron asper ur need xD 
+
+mkdir -p /tmp/rom
+
+cd /tmp/rom
+
+sudo apt install git -y
+
+repo init --depth=1 -u git://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault 
+
+repo sync -j$(nproc --all)
+
+. build/envsetup.sh && lunch nad_violet-eng && mka nad
+
+# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
+
+cd out/target/product/violet
+
+curl -sL https://git.io/file-transfer | sh 
+
+./transfer wet Nusan*.zip
