37a3fdc0bf796cc1248bfd660c590f743d08a3b1
brucetech627 <66194009+brucetech627@users.noreply.github.com>
2021-05-04 19:18:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ cd /tmp/rom
 
 #tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')
 
-apt install tmate
+sudo apt install tmate -A
 
 export TERM=xterm
 
